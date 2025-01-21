01a15a1b167df24a09326fc9956bbe01b0f20f06
lucasnahuels <lucasnahuelstp@gmail.com>
2025-01-21 07:52:20 -0300

    Implementation of Angular Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { PersonList } from './components/person/person-list/person-list.component';
 import { PersonEdit } from './components/person/person-edit/person-edit.component';
 import { PersonCreate } from './components/person/person-create/person-create.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: 'list', component: PersonList },
-  { path: 'update/:id', component: PersonEdit },
-  { path: 'create', component: PersonCreate }
+  { path: 'list', component: PersonList, canActivate: [AuthGuard] },
+  { path: 'update/:id', component: PersonEdit, canActivate: [AuthGuard] },
+  { path: 'create', component: PersonCreate, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
