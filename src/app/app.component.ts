3c089750519959b87a80bbe4efce48453c79d588
lucasnahuels <lucasnahuelstp@gmail.com>
2025-01-26 07:47:47 -0300

    Working on the login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp2.client';
+
+  constructor(public authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
